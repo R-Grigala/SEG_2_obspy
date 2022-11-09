c242b6bd09a29dea8a050c91057294d834a29baf
R-Grigala <r.grigalashvili777@gmail.com>
2022-11-10 01:26:17 +0400

    Check Writing

--- convert_seg2_to_segy.py
+++ convert_seg2_to_segy.py
@@ -1,11 +1,24 @@
-#ბიბლიოთეკების ინიციალიზაცია
+# ბიბლიოთეკების ინიციალიზაცია
 import obspy
 from obspy import read
 
-#პირველი ჩანაწერის წაკითხვა
+# პირველი ჩანაწერის წაკითხვა
 first_data_stream = read("./Data/101.dat")
 # print(first_data_stream)
 
-#მეორე ჩანაწერის წაკითხვა
+# მეორე ჩანაწერის წაკითხვა
 second_data_stream = read('./Data/102.dat')
-print(second_data_stream)
+# print(second_data_stream)
+
+# ორი ჩანაწერის გადასაბმელად საჭიროა მეორე ჩანაწერის დაწყების დრო ემთხვეოდეს 
+# პირველი ჩანაწერის დასრულების დროს
+for i in range(len(second_data_stream)):
+    second_data_stream[i].stats['starttime'] = first_data_stream[0].stats['endtime']
+
+# ორი ჩანაწერის გაერთიანება: Trace მონაცემებით
+both_stream = first_data_stream + second_data_stream
+# print(both_stream[45].stats)
+# both_stream.plot()
+
+# გაერთიანებული მონაცემების ფაილში ჩაწერა
+both_stream.write("both_stream.sgy", format="SEGY")
